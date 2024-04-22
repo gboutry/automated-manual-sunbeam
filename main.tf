cf2abb25fa4d28bdedbd429e0fd8294e2e7ff4fe
Guillaume Boutry <guillaume.boutry@canonical.com>
2024-04-22 16:32:48 +0200

    push up...

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ variable "nb_vm" {
 }
 
 resource "lxd_volume" "osd" {
-  count        = 3 * var.nb_vm
+  count        = 4 * var.nb_vm
   name         = "osd${count.index}"
   pool         = "default"
   type         = "custom"
@@ -31,57 +31,35 @@ resource "lxd_network" "baremetal" {
   name = "bmbr"
 
   config = {
-    "ipv4.address" = "10.20.20.1/24"
+    "ipv4.address" = "10.20.30.1/24"
     "ipv4.nat"     = "true"
+    "ipv4.dhcp"    = "false"
     "ipv6.address" = "none"
   }
 }
 
-# resource "lxd_profile" "microk8s" {
-#   name = "microk8s"
+resource "lxd_instance" "baremetal" {
+  count      = var.nb_vm
+  name       = "bm${count.index}"
+  image      = "ubuntu:jammy"
+  type       = "virtual-machine"
 
-#   config = {
-#     "linux.kernel_modules" = "ip_vs,ip_vs_rr,ip_vs_wrr,ip_vs_sh,ip_tables,ip6_tables,netlink_diag,nf_nat,overlay,br_netfilter"
-#     "raw.lxc"              = <<-EOT
-#       lxc.apparmor.profile=unconfined
-#       lxc.mount.auto=proc:rw sys:rw cgroup:rw
-#       lxc.cgroup.devices.allow=a
-#       lxc.cap.drop=
-#     EOT
-#     "security.nesting"     = "true"
-#     "security.privileged"  = "true"
-#   }
-#   device {
-#     name = "kmsg"
-#     type = "unix-char"
-#     properties = {
-#     path = "/dev/kmsg"
-#     source = "/dev/kmsg"
-#     }
-#   }
-# }
-
-resource "lxd_container" "baremetal" {
-  count = var.nb_vm
-  name  = "bm${count.index}"
-  image = "images:ubuntu/22.04/cloud"
-  type  = "virtual-machine"
-
-  # profiles = [lxd_profile.microk8s.name]
   limits = {
-    cpu    = "12"
-    memory = "31GiB"
+    cpu    = "8"
+    memory = count.index == 0 ? "30GiB" : "13GiB"
   }
 
   config = {
     "user.access_interface" = "eth0"
     "user.user-data"        = templatefile("${path.root}/cloudinit.yaml", { index = count.index })
+    "user.network-config"   = file("${path.root}/network.yaml")
   }
 
   device {
     name = "eth0"
     type = "nic"
     properties = {
+      name           = "eth0"
       network        = "lxdbr0"
       "ipv4.address" = "10.206.54.${40 + count.index}"
     }
@@ -92,9 +70,11 @@ resource "lxd_container" "baremetal" {
     type = "nic"
 
     properties = {
+      name    = "eth1"
       network = lxd_network.baremetal.name
     }
   }
+
 
   device {
     name = "root"
@@ -102,7 +82,7 @@ resource "lxd_container" "baremetal" {
     properties = {
       path = "/"
       pool = "default"
-      size = "150GB"
+      size = "100GiB"
     }
   }
 
@@ -132,8 +112,17 @@ resource "lxd_container" "baremetal" {
       source = lxd_volume.osd[count.index * 3 + 2].name
     }
   }
+
+  device {
+    name = lxd_volume.osd[count.index * 3 + 3].name
+    type = "disk"
+    properties = {
+      pool   = "default"
+      source = lxd_volume.osd[count.index * 3 + 3].name
+    }
+  }
 }
 
 output "baremetal_ip" {
-  value = lxd_container.baremetal[*].ipv4_address
+  value = lxd_instance.baremetal[*].ipv4_address
 }
